9880f09000bb19d6f327506c34ad8e7223a5b59f
Kevin Conley <kevindconley@gmail.com>
2015-05-29 16:19:05 -0700

    Inserted #define's that strip out parts of gnuboy into code directly.

--- src/defs.h
+++ src/defs.h
@@ -1,10 +1,16 @@
+#pragma once
 
-
-
-#ifndef __DEFS_H__
-#define __DEFS_H__
-
-
+#define NO_NOISE15
+#define NO_PALETTE
+#define GNUBOY_NO_MINIZIP
+#define GNUBOY_NO_PRINTF
+#define GNUBOY_NO_SCREENSHOT
+#define NODEBUG
+#define GNUBOY_NOSAVELOAD
+#define BITBOY_FIXEDRAM 4
+#define BITBOY_NOCGB
+#define IS_LITTLE_ENDIAN
+#define NOTITLE
 
 #ifdef IS_LITTLE_ENDIAN
 #define LO 0
@@ -13,7 +19,6 @@
 #define LO 1
 #define HI 0
 #endif
-
 
 typedef unsigned char byte;
 
@@ -27,10 +32,3 @@ typedef signed int n32;
 
 typedef un16 word;
 typedef word addr;
-
-
-
-
-
-#endif
-
